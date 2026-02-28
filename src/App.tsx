6b32b60eaa1804ff2b97171989ed63c0bee551a1
DTN101 <dileepkumar.tentu@serviceaide.com>
2026-02-28 13:15:38 +0530

    Changes and fixes.

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (username === "user" && password === "password") {
+      setMessage("Welcome, user!");
+    } else {
+      setMessage("Invalid username or password");
+    }
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <br />
+          <input
+            type="text"
+            id="username"
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <br />
+          <input
+            type="password"
+            id="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <br />
+
+        <button type="submit">Submit</button>
+      </form>
+
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
